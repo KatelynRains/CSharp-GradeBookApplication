369130a765827dd5b75451c0dc5a43573bb0ed21
Katelyn Rains <KRains@tql.com>
2021-09-08 16:08:35 -0400

    updated constructor

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
 
 
         public StandardGradeBook(string name) : base(name)
-        { 
+        {
+            Type = GradeBookType.Standard;
         }
     }
 }
